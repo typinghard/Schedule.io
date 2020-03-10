0605c0b8ea8f3888ba86b6947b923d24450ca385
Diego Galante <diegogalantte@gmail.com>
2020-03-10 19:16:54 -0300

    Adicionando Solução Nuget. Adicionando pasta Nugets e algumas versões de teste.

---
+++ Agenda.Nuget/AgendaAppliationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Agenda.Infra.CrossCutting.IoC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NugetAgenda.Nuget
+{
+    public static class AgendaAppliationBuilderExtensions
+    {
+        public static IApplicationBuilder UseAgenda(this IApplicationBuilder app)
+        {
+            return app;
+        }
+
+        public static IServiceCollection AddAgenda(this IServiceCollection services)
+        {
+            NativeInjectorBootStrapper.RegisterServices(services);
+            return services;
+        }
+
+
+    }
+}
---
+++ Agenda.UI.Web/Controllers/AgendaController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agenda.UI.Web.Controllers
+{
+    public class AgendaController : Controller
+    {
+        // GET: Agenda
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Agenda/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Agenda/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agenda/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Agenda/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Agenda/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Agenda/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Agenda/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

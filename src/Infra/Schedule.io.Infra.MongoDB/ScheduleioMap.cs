cb3a2a4748eca8db4978a46b1cdf68fdfacff583
Elvis Souza <elviscristiansouza@gmail.com>
2020-04-14 14:35:00 -0300

    Adequações nos Repos RavenDb e MongoDb pt.2

---
+++ src/Infra/Schedule.io.Infra.MongoDB/ScheduleioMap.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization;
+using Schedule.io.Models.AggregatesRoots;
+
+namespace Schedule.io.Infra.MongoDB
+{
+    public class ScheduleioMap : MongoMap<Evento>
+    {
+        public ScheduleioMap()
+        {
+            if (!IsClassMapRegistered())
+            {
+                BsonClassMap.RegisterClassMap<Evento>(cm =>
+                {
+                    cm.AutoMap();
+                });
+            }
+        }
+    }
+}
---
+++ src/Infra/Schedule.io.Infra.MongoDB/TipoEventoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Schedule.io.Interfaces.Repositories;
+using Schedule.io.Models.AggregatesRoots;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.io.Infra.MongoDB
+{
+    public class TipoEventoRepository : Repository<TipoEvento>, ITipoEventoRepository
+    {
+        public TipoEventoRepository(ScheduleioContext context) : base(context)
+        {
+        }
+    }
+}

f80c6c8e656d4836393cb107ae76e47c5f07c61d
Diego Galante <diegogalantte@gmail.com>
2020-04-20 21:45:12 -0300

    Remoção da classe helper GuidExtension. Alteração dos nomes dos metodos na classe StringExtensionTests.

--- tests/Scheduleio.Domain.Test/Testes Unitários/Core/Helpers/StringExtensionsTests.cs
+++ tests/Scheduleio.Domain.Test/Testes Unitários/Core/Helpers/StringExtensionsTests.cs
@@ -8,21 +8,21 @@ namespace Schedule.io.Test.Testes_Unitários.Core.Helpers
 {
     public class StringExtensionsTests
     {
-        [Theory(DisplayName = "StringExtension - EhVazio - Deve Ser Válido")]
+        [Theory(DisplayName = "StringExtension - EhVazio - Deve Ser True")]
         [InlineData("")]
         [InlineData(" ")]
-        public void StringExtension_EhVazio_DeveSerValido(string str)
+        public void StringExtension_EhVazio_DeveSerTrue(string str)
         {
             var result = str.EhVazio();
 
             Assert.True(result);
         }
 
-        [Theory(DisplayName = "StringExtension - EhVazio - Deve Ser Inválido")]
+        [Theory(DisplayName = "StringExtension - EhVazio - Deve Ser False")]
         [InlineData(".")]
         [InlineData(" .")]
         [InlineData(". ")]
-        public void StringExtension_EhVazio_DeveSerInvalido(string str)
+        public void StringExtension_EhVazio_DeveSerFalse(string str)
         {
             var result = str.EhVazio();
 
@@ -30,30 +30,30 @@ namespace Schedule.io.Test.Testes_Unitários.Core.Helpers
         }
 
 
-        [Theory(DisplayName = "StringExtension - ValidarTamanho - Deve Ser Válido")]
+        [Theory(DisplayName = "StringExtension - ValidarTamanho - Deve Ser True")]
         [InlineData("abcde", 1, 5)]
         [InlineData("a", 1, 1)]
         [InlineData("abcde", 5, 5)]
         [InlineData("a", -1, 1)]
-        public void StringExtension_ValidarTamanho_DeveSerValido(string str, int tamanhoMinimo, int tamanhoMaximo)
+        public void StringExtension_ValidarTamanho_DeveSerTrue(string str, int tamanhoMinimo, int tamanhoMaximo)
         {
             var result = str.ValidarTamanho(tamanhoMinimo, tamanhoMaximo);
 
             Assert.True(result);
         }
 
-        [Theory(DisplayName = "StringExtension - ValidarTamanho - Deve Ser Inválido")]
+        [Theory(DisplayName = "StringExtension - ValidarTamanho - Deve Ser False")]
         [InlineData("", 1, 5)]
         [InlineData("abcde", 6, 5)]
         [InlineData("a", -1, 0)]
-        public void StringExtension_ValidarTamanho_DeveSerInvalido(string str, int tamanhoMinimo, int tamanhoMaximo)
+        public void StringExtension_ValidarTamanho_DeveSerFalse(string str, int tamanhoMinimo, int tamanhoMaximo)
         {
             var result = str.ValidarTamanho(tamanhoMinimo, tamanhoMaximo);
 
             Assert.False(result);
         }
 
-        [Theory(DisplayName = "StringExtension - EmailEhValido - Deve Ser Válido")]
+        [Theory(DisplayName = "StringExtension - EmailEhValido - Deve Ser True")]
         [InlineData("david.jones@proseware.com")]
         [InlineData("d.j@server1.proseware.com")]
         [InlineData("jones@ms1.proseware.com")]
@@ -67,7 +67,7 @@ namespace Schedule.io.Test.Testes_Unitários.Core.Helpers
         [InlineData("someone+tag@somewhere.net")]
         //[InlineData("futureTLD@somewhere.fooo")]
         //[InlineData("js@contoso.中国")]
-        public void StringExtension_EmailEhValido_DeveSerValido(string str)
+        public void StringExtension_EmailEhValido_DeveSerTrue(string str)
         {
             var result = str.EmailEhValido();
 
@@ -75,7 +75,7 @@ namespace Schedule.io.Test.Testes_Unitários.Core.Helpers
         }
 
 
-        [Theory(DisplayName = "StringExtension - EmailEhValido - Deve Ser Inválido")]
+        [Theory(DisplayName = "StringExtension - EmailEhValido - Deve Ser False")]
         [InlineData("j.@server1.proseware.com")]
         [InlineData("j..s@proseware.com")]
         [InlineData("js*@proseware.com")]
@@ -84,7 +84,7 @@ namespace Schedule.io.Test.Testes_Unitários.Core.Helpers
         [InlineData("fdsa@")]
         [InlineData("fdsa@fdsa")]
         [InlineData("fdsa@fdsa.")]
-        public void StringExtension_EmailEhValido_DeveSerInvalido(string str)
+        public void StringExtension_EmailEhValido_DeveSerFalse(string str)
         {
             var result = str.EmailEhValido();
 
